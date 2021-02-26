0f0b07bf11149ff4cca88009f931126f1f0d35c8
rubashkin_mikhail@tut.by <pristijpribor@mail.ru>
2021-02-26 11:12:47 +0300

    Sidebar changes

--- resources/views/inc/aside.blade.php
+++ resources/views/inc/aside.blade.php
@@ -1,6 +1,22 @@
 @section('aside')
 <div class="aside">
   <h4>Боковая панель</h4>
+  <div class="wrapper">
+    <div class="cube">
+      <b class="front">Ваша рекламма</b>
+      <b class="back"></b>
+      <b class="top"></b>
+      <b class="bottom"></b>
+      <b class="left"></b>
+      <b class="right"></b>
+      <i class="front"></i>
+      <i class="back"></i>
+      <i class="top"></i>
+      <i class="bottom"></i>
+      <i class="left"></i>
+      <i class="right"></i>
+    </div>
+  </div>
   <p>Это просто боковая панель</p>
   <div class="wrapper">
     <div class="cube">
